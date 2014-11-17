403b774f70db9397b8dfaa181859c1d98e9c8c9e
root on vernacolo01 <root@vernacolo01.softecspa.it>
2014-11-17 11:09:49 +0100

    aggiunte env vars

--- manifests/management.pp
+++ manifests/management.pp
@@ -1,9 +1,10 @@
 class rabbitmq::management inherits rabbitmq::params {
 
     exec {'rabbitmq-plugins-management':
-      command => '/usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_management',
-      unless => '/usr/lib/rabbitmq/bin/rabbitmq-plugins list | egrep "^\[E] rabbitmq_management"',
-      require => [ Package['rabbitmq-server'], File['rabbitmq.config'] ],
+      command     => '/usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_management',
+      unless      => '/usr/lib/rabbitmq/bin/rabbitmq-plugins list | egrep "^\[E] rabbitmq_management"',
+      environment => ['HOME=/root'],
+      require     => [ Package['rabbitmq-server'], File['rabbitmq.config'] ],
     }
 
     file { '/usr/local/bin/rabbitmqadmin':
